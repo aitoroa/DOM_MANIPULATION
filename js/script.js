2ef9bc3da16b726c878532a50144d7e8cb6b899a
Aitor Ochoa <aitorochoa10@gmail.com>
2025-10-24 19:25:07 +0200

    switch theme

--- js/script.js
+++ js/script.js
@@ -12,3 +12,22 @@ burger.addEventListener("click", function(){
     burger.classList.toggle("fa-times");
     menu_opt.classList.toggle("menu-show");
 });
+
+// Cambio de modo
+
+let btnSwitchMode = document.querySelector(".theme-switch");
+btnSwitchMode.addEventListener("click", function(){
+    let body = document.querySelector("body");
+    let to_dark_mode = document.querySelector(".to-dark-mode");
+    let to_light_mode = document.querySelector(".to-light-mode");
+    let condition = body.classList.contains("dark-theme");
+    if (condition) {
+        body.classList.remove("dark-theme");
+        body.classList.add("light-theme");
+    } else {
+        body.classList.remove("light-theme");
+        body.classList.add("dark-theme");
+    }
+    to_light_mode.classList.toggle("hide-cont");
+    to_dark_mode.classList.toggle("hide-cont");
+});
